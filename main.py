ea5142e79612a3f0bc17e2707685869a0f76db6d
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-18 22:52:23 -0300

    Including new method to find images based on the given text.

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from google_images_search import GoogleImagesSearch as externalApi
 
 
-def print_hi(name):
+def search_image(image_to_search):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    gis = externalApi('api_key', 'cx')
+    gis.search({'q': 'bookshelf', 'num': 10})
 
-    print('tototototo')
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    search_image('PyCharm')
